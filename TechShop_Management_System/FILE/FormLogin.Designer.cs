17b900b4dee04c0dbc556500c38e4841669c2f26
Sujan-19070 <122681709+Sujan-19070@users.noreply.github.com>
2025-02-02 18:42:55 +0600

    menu edit

--- TechShop_Management_System/FILE/FormLogin.Designer.cs
+++ TechShop_Management_System/FILE/FormLogin.Designer.cs
@@ -49,9 +49,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(132, 239);
+            this.label1.Location = new System.Drawing.Point(176, 294);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.Size = new System.Drawing.Size(84, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "USERNAME";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -59,9 +60,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(132, 309);
+            this.label2.Location = new System.Drawing.Point(176, 380);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.Size = new System.Drawing.Size(86, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "PASSWORD";
             // 
@@ -70,9 +72,10 @@
             this.label3.AutoSize = true;
             this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(193, 382);
+            this.label3.Location = new System.Drawing.Point(257, 470);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.Size = new System.Drawing.Size(170, 25);
             this.label3.TabIndex = 3;
             this.label3.Text = "Forgot Password?";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -80,16 +83,18 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(135, 268);
+            this.textBox1.Location = new System.Drawing.Point(180, 330);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(237, 25);
+            this.textBox1.Size = new System.Drawing.Size(315, 29);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(135, 337);
+            this.textBox2.Location = new System.Drawing.Point(180, 415);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(237, 20);
+            this.textBox2.Size = new System.Drawing.Size(315, 22);
             this.textBox2.TabIndex = 5;
             // 
             // pictureBox3
@@ -97,9 +102,10 @@
             this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox3.Image = global::TechShop_Management_System.Properties.Resources.pngtree_view_icon_png_image_5196447;
-            this.pictureBox3.Location = new System.Drawing.Point(378, 337);
+            this.pictureBox3.Location = new System.Drawing.Point(504, 415);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(25, 20);
+            this.pictureBox3.Size = new System.Drawing.Size(33, 25);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
@@ -110,9 +116,10 @@
             this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = global::TechShop_Management_System.Properties.Resources.png_transparent_computer_icons_finance_drawing_hide_logo_smiley_глаз_icon;
-            this.pictureBox2.Location = new System.Drawing.Point(378, 337);
+            this.pictureBox2.Location = new System.Drawing.Point(504, 415);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(25, 20);
+            this.pictureBox2.Size = new System.Drawing.Size(33, 25);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
@@ -122,9 +129,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.pictureBox1.Image = global::TechShop_Management_System.Properties.Resources.images;
-            this.pictureBox1.Location = new System.Drawing.Point(176, 40);
+            this.pictureBox1.Location = new System.Drawing.Point(235, 49);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(183, 182);
+            this.pictureBox1.Size = new System.Drawing.Size(244, 224);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -134,9 +142,10 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(145, 458);
+            this.button1.Location = new System.Drawing.Point(193, 564);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(227, 32);
+            this.button1.Size = new System.Drawing.Size(303, 39);
             this.button1.TabIndex = 8;
             this.button1.Text = "Log In";
             this.button1.UseVisualStyleBackColor = false;
@@ -144,9 +153,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Location = new System.Drawing.Point(16, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(223, 13);
+            this.label4.Size = new System.Drawing.Size(278, 16);
             this.label4.TabIndex = 9;
             this.label4.Text = "Computer Shop Management System | Log In ";
             // 
@@ -154,19 +164,20 @@
             // 
             this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.pictureBox4.Image = global::TechShop_Management_System.Properties.Resources.pngtree_monochrome_computer_icon_for_web_design_and_infographics_vector_png_image_40639554;
-            this.pictureBox4.Location = new System.Drawing.Point(472, 9);
+            this.pictureBox4.Location = new System.Drawing.Point(629, 11);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(73, 66);
+            this.pictureBox4.Size = new System.Drawing.Size(97, 81);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 10;
             this.pictureBox4.TabStop = false;
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(567, 560);
+            this.ClientSize = new System.Drawing.Size(756, 689);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
@@ -179,6 +190,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Computer Shop Management System | Log In";
